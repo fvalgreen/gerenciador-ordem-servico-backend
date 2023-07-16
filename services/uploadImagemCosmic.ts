c71f8a79e4f75dd4a677a2b2ff9809b27b63ab58
fvalgreen <valverde130495@gmail.com>
2023-07-16 00:02:40 -0300

    ajuste cosmic

--- services/uploadImagemCosmic.ts
+++ services/uploadImagemCosmic.ts
@@ -1,33 +1,36 @@
+import { createBucketClient } from "@cosmicjs/sdk";
 import multer from "multer";
-import cosmicjs from "cosmicjs";
 
-const {
-  CHAVE_GRAVACAO_CHAMADOS,
-  BUCKET_CHAMADOS,
-} = process.env;
+const { BUCKET_SLUG, READ_KEY, WRITE_KEY } = process.env;
 
-const Cosmic = cosmicjs();
-const bucketChamados = Cosmic.bucket({
-  slug: BUCKET_CHAMADOS,
-  write_key: CHAVE_GRAVACAO_CHAMADOS
+const bucketDevagram = createBucketClient({
+  bucketSlug: BUCKET_SLUG as string,
+  readKey: READ_KEY as string,
+  writeKey: WRITE_KEY as string,
 });
 
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+
+const upload = multer({ storage: storage });
 
 const uploadImagemCosmic = async (req: any) => {
-  if(req?.file?.originalname){
-    if(!req.file.originalname.includes('.png') && !req.file.originalname.includes('.jpg') && !req.file.originalname.includes('.jpeg')){
-      throw new Error('Extensão de imagem inválida');
+  if (req?.file?.originalname) {
+    if (
+      !req.file.originalname.includes(".png") &&
+      !req.file.originalname.includes(".jpg") &&
+      !req.file.originalname.includes(".jpeg")
+    ) {
+      throw new Error("Extensão da imagem inválida");
     }
-
     const media_object = {
       originalname: req.file.originalname,
-      buffer: req.file.buffer
-    }
-
-    return await bucketChamados.addMedia({media: media_object});;
-  };
+      buffer: req.file.buffer,
+    };
+    return await bucketDevagram.media.insertOne({
+      media: media_object,
+      folder: "publicacao",
+    });
+  }
 };
 
-export {upload, uploadImagemCosmic};
+export { upload, uploadImagemCosmic };
